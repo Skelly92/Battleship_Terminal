5d655140bd67e4112363faff86f41d56fcfa046e
Skelly92 <80195636+Skelly92@users.noreply.github.com>
2021-05-15 12:28:23 +0100

    input of name of user

--- Battleship_Terminal.py
+++ Battleship_Terminal.py
@@ -1,1 +1,12 @@
+class Battleship():
+    def __init__(self):
+            for i in range(10):
+                print(" ")
 
+    def name(self):
+        self = input("What is your name? ")
+        return self
+
+
+x = Battleship()
+x.name()
